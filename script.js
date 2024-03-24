25d3f101633f9926efab05c0a656e33fcf1eb810
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-23 22:36:00 -0300

    add: function for reset

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let correctAnswer = 0;
 
 showQuestion();
 
+// Events
+document.querySelector('.scoreArea button').addEventListener('click',resetEvent)
+
 // Function
 function showQuestion() {
     if (questions[currentQuestion]) {
@@ -68,4 +71,10 @@ function finishQuiz() {
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.progress--bar').style.width = `100%`;
 
+}
+
+function resetEvent() {
+    correctAnswer = 0
+    currentQuestion = 0
+    showQuestion()
 }
