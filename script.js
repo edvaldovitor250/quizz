43b023f5ef2f0caace58d4fcb7e97a56d296021e
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-03-23 22:33:09 -0300

    add: function for finish quiz

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ function showQuestion() {
         let q = questions[currentQuestion];
 
         let pct = Math.floor((currentQuestion + 1) / questions.length * 100);
-        
+
         document.querySelector('.progress--bar').style.width = `${pct}%`;
-
-
-
 
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
@@ -24,16 +21,16 @@ function showQuestion() {
 
         let optionsHtml = '';
         for (let i in q.options) {
-            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span>${q.options[i]}</div>`;
         }
         document.querySelector('.options').innerHTML = optionsHtml;
 
-        document.querySelectorAll('.option').forEach(item =>{
+        document.querySelectorAll('.option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
         });
 
     } else {
-       finishQuiz();
+        finishQuiz();
     }
 }
 
@@ -44,8 +41,31 @@ function optionClickEvent(e) {
 
     if (clickedOptionIndex === correctAnswerIndex) {
         correctAnswer++;
-        
+
     }
     currentQuestion++;
-    showQuestion(); 
+    showQuestion();
+}
+
+function finishQuiz() {
+    let points = Math.floor((correctAnswer / questions.length) *100)
+    
+    if(points < 30){
+        document.querySelector('.scoreText1').innerHTML= `Tá ruim em!!`
+        document.querySelector('.scorePct').style.color = '#FF0000'
+    } else if(points>=30 && points <70){
+        document.querySelector('.scoreText1').innerHTML= `Muito bom!`
+        document.querySelector('.scorePct').style.color = '#FFFF00'
+    } else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML= `Parabéns, Você foi ótimo`
+        document.querySelector('.scorePct').style.color = '#0D630D'
+    }
+    
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`
+    document.querySelector('.scoreText2').innerHTML= `Você respondeu ${questions.length} questões e acertou ${correctAnswer}`
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = `100%`;
+
 }
